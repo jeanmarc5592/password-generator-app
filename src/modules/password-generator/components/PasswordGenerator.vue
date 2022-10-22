d4c547e22ed63e3c54f1a1b5ab27d33abb9b71d9
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-10-22 22:17:02 +0200

    Add Heading and Result (wip)

--- src/modules/password-generator/components/PasswordGenerator.vue
+++ src/modules/password-generator/components/PasswordGenerator.vue
@@ -1,12 +1,51 @@
 <template>
   <div class="main-container">
-    <base-typography>Password Generator App</base-typography>
+    <!-- HEADING -->
+    <div class="heading-container">
+      <base-typography>Password Generator</base-typography>
+    </div>
+    <!-- PASSWORD RESULT -->
+    <div class="result-container">
+      <base-typography variant="heading-large">
+        {{ password }}
+      </base-typography>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      generatedPassword: "",
+    };
+  },
+  computed: {
+    password() {
+      return this.generatedPassword.length > 0
+        ? this.generatedPassword
+        : "P4$5W0rD!";
+    },
+  },
+};
+</script>
+
 <style lang="scss" scoped>
 .main-container {
+  width: 343px;
+  @media (min-width: 768px) {
+    width: 540px;
+  }
+}
+
+.heading-container {
+  margin-bottom: 2rem;
+  text-align: center;
+  color: $color-grey-dark;
+}
+
+.result-container {
   background-color: $color-black-light;
-  max-width: 540px;
+  padding: 1rem 2rem;
 }
 </style>
