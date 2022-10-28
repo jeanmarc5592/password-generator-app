69a365f417c202e177e489df5444bd2789898db7
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-10-28 21:31:29 +0200

    Add BaseArrowRightIcon Component

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import "./styles/main.scss";
 
 import BaseTypography from "./common/components/BaseTypography.vue";
 import BaseCopyIcon from "./common/components/BaseCopyIcon.vue";
+import BaseArrowRightIcon from "./common/components/BaseArrowRightIcon.vue";
 
 const app = createApp(App);
 
 app.component("BaseTypography", BaseTypography);
 app.component("BaseCopyIcon", BaseCopyIcon);
+app.component("BaseArrowRightIcon", BaseArrowRightIcon);
 
 app.mount("#app");
